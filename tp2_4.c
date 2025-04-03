4f92034fb436a8b5304b02dddefd06bf66810b6d
Lucas <Lucasfrx432@gmail.com>
2025-04-02 22:01:10 -0300

    avanzando en el punto 4 version 2

--- tp2_4.c
+++ tp2_4.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
-struct compu { int velocidad; // Velocidad de procesamiento en GHz (valor entre 1 y 3) 
-int anio; // Año de fabricación (valor entre 2015 y 2024) 
-int cantidad_nucleos; // Cantidad de núcleos (valor entre 1 y 8) 
-char *tipo_cpu; // Tipo de procesador (apuntador a cadena de caracteres) 
-};
+struct compu { 
+    int velocidad; // Velocidad de procesamiento en GHz (valor entre 1 y 3) 
+    int anio; // Año de fabricación (valor entre 2015 y 2024) 
+    int cantidad_nucleos; // Cantidad de núcleos (valor entre 1 y 8) 
+    char *tipo_cpu; // Tipo de procesador (apuntador a cadena de caracteres) 
+}typedef compu;
 
 int main(){
-struct compu
+    compu compu1, compu2, compu3, compu4, compu5;
 {
     /* data */
 };
