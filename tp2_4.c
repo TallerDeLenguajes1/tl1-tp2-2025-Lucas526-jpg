ed738759b296454229f3755b7ba7213b9ed48fb1
Lucas <Lucasfrx432@gmail.com>
2025-04-02 23:40:10 -0300

    avazando en el punto 4 version 4

--- tp2_4.c
+++ tp2_4.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct compu { 
     int velocidad; // Velocidad de procesamiento en GHz (valor entre 1 y 3) 
@@ -10,12 +11,29 @@ struct compu {
 int main(){
     compu compu1, compu2, compu3, compu4, compu5;
     char tipos[6][10] = {"Intel", "AMD", "Celeron", "Athlon", "Core", "Pentium"};
-{
+    int filaAleatorio = rand() % 6;
+    int columnaAleatorio = rand() % 10;
+
     /* genero los datos aleatorios de compu 1 */
-    compu1.velocidad = 1 + rand() % 4;
-    compu1.anio = 2015 + rand() % 2025;
-    compu1.cantidad_nucleos = 1 + rand() % 9;
+    compu1.velocidad = rand() % 3 + 1;
+    compu1.anio = rand() % 11 + 2015;
+    compu1.cantidad_nucleos = rand() % 9 + 1;
+    compu1.tipo_cpu = &tipos[filaAleatorio][columnaAleatorio];
 
-};
+    printf("Velocidad: %d\n",compu1.velocidad);
+    printf("Anio: %d\n",compu1.anio);
+    printf("Cantidad de nucleos: %d\n",compu1.cantidad_nucleos);
+    printf("Tipo cpu: %c\n",*compu1.tipo_cpu);
 
+// for (int i = 0; i < 6; i++)
+// {
+//     for (int j = 0; j < 10; j++)
+//     {
+//         printf("arreglo[%d][%d]: %c\n",i,j,tipos[i][j]);
+
+//     }
+    
+// }
+
+    
 }
